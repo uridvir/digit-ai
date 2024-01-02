e53f622568ef53f872ed122d80fd008b882bd738
Uri Dvir <uri.j.dvir@gmail.com>
2024-01-01 21:10:18 -0500

    Added contrast filter to webcam

--- webcam.py
+++ webcam.py
@@ -28,14 +28,24 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = ~cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    image = ~cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    alpha = 1.5 # Contrast control (1.0-3.0)
+    beta = 0 # Brightness control (0-100)
+    image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+    _, image = cv.threshold(image, 200, 255, cv.THRESH_TOZERO)
+
+    # Crop center third of image
+    w, h = image.shape
+    image = image[int(w/3):int(2*w/3), int(h/3):int(2*h/3)]
 
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame', image)
     if cv.waitKey(1) == ord(' '):
         """Feed frame into neural net"""
 
-        image = cv.resize(gray, (28, 28))
+        image = cv.resize(image, (28, 28))
         plt.imshow(image)
         plt.show()
         data = transforms.ToTensor()(image)
