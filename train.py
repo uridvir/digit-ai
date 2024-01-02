1879dd08252b9a703d561051cc0133a59b7b06a5
Uri Dvir <uri.j.dvir@gmail.com>
2024-01-01 19:26:22 -0500

    Added webcam

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        self.conv2_drop = nn.Dropout1d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
 
@@ -48,7 +48,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
 
 def train():
     """Get the training data"""
--- webcam.py
+++ webcam.py
@@ -1,1 +1,46 @@
-print("Hello world!")
+from train import Net
+import torch
+from torchvision import transforms
+from matplotlib import pyplot as plt
+
+import cv2 as cv
+
+"""Import the neural network"""
+net = Net()
+net.load_state_dict(torch.load("net.pth"))
+
+
+"""Get input from the webcam"""
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = ~cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord(' '):
+        """Feed frame into neural net"""
+
+        image = cv.resize(gray, (28, 28))
+        plt.imshow(image)
+        plt.show()
+        data = transforms.ToTensor()(image)
+        response = input("Use this image (y/n)?")
+        if response == "y":
+            print("Net result is ", torch.max(net(data), 1)[1][0].item())
+            plt.imshow(image)
+            plt.show()
+        plt.close()
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
